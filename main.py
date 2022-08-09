b23c08f00cade7ca2ec15af6424da0115af99554
Gabriel Fontanez <41862878+Konpy@users.noreply.github.com>
2022-08-09 18:26:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from tkinter import *
 from pytube import YouTube
+import time
 
-
+#Make the program window for everyone to see
 root = Tk()
-root.geometry = ("500x400")
+root.geometry = ("800x600")
 root.title("Youtube Downloader")
 
-def Video_links():
-    INPUT = inputtxt.get("1.0", "end-1c")
-    try:
-        good_apple = YouTube(INPUT)
+#Code to grab video links and download them as mp4/3
 
-        #Video Information
-        Output.insert(END,f"Downloading:\n {good_apple.title}")
-        good_apple.streams.get_highest_resolution().download("C:/Users/danka/Downloads",f"{good_apple.title}.mp4")
-        Output.insert(END,"\nDone loading")
-    except:
-        Output.insert(END, "\nError raised\n")
-        print("Hi error its ya boy")
+def splitURL(string):
+    print (string)
+    return list(string.split('~'))
+
+def Video_links():
+    #This is imput from the user after they press a button
+    INPUT = inputtxt.get("1.0", "end-1c")
+    url = splitURL(INPUT)
+    for i in url:
+        try:
+        #Name of the youtube title to download being saved as Good_apple
+            good_apple = YouTube(i)
+            #Video Information
+            Output.insert(END,f"\nDownloading:\n {good_apple.title}")
+            time.sleep(2)
+            good_apple.streams.get_highest_resolution().download("C:/Users/danka/Downloads",f"{good_apple.title}.mp4")
+            Output.insert(END,"\nDone loading\n")
+        except: 
+            Output.insert(END, "\nError raised\n")
+            print("Goodapple isnt so good. But the files load in DL folder somehow who cares")
+        inputtxt.delete("1.0", "end-1c")
+
 def Audio_links():
     INPUT = inputtxt.get("1.0", "end-1c")
     try:
@@ -28,23 +41,26 @@ def Audio_links():
         good_apple.streams.get_audio_only().download("C:/Users/danka/Downloads",f"{good_apple.title}.mp3")
         Output.insert(END,"\nDone loading")
     except:
+        Output.insert(END, "\nError raised\n")
         print("Hi error its ya boy")
-    return;
+    inputtxt.delete("1.0", "end-1c")
+
+
 
 l = Label(text = "Youtube links go here ")
 inputtxt = Text(root, height = 10,
-                width = 25,
+                width = 45,
                 bg = "light yellow")
- 
+
 Output = Text(root, height = 5,
-              width = 25,
+              width = 45,
               bg = "light cyan")
  
-Display1 = Button(root, height = 2,
+Display1 = Button(root, height = 1,
                  width = 20,
                  text ="Download Vids",
                  command = lambda:Video_links())
-Display2 = Button(root, height = 2,
+Display2 = Button(root, height = 1,
                  width = 20,
                  text ="Download Music",
                  command = lambda:Audio_links())
